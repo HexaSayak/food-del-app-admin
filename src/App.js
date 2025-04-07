b8ac28811cd8bf1d98d960857d0253e5405ee069
HexaSayak <sayakghosh24012001@gmail.com>
2025-04-07 13:24:10 +0530

    complete admin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
+const App = () => {
+
+  // const url = "http://localhost:4000";
+  const url = "https://food-del-app-backend-x861.onrender.com";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path="/orders" element={<Orders url={url}/>}/>
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
